aedbba63150fc0d8653465702b7061a23de629e0
Simon Wekesa <wekesawafula15@gmail.com>
2024-05-08 19:39:39 +0300

    settings updated to include the new apps

--- campusvibes/settings.py
+++ campusvibes/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Users',
     'channels',
+    'Admin',
+    'superAdmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
